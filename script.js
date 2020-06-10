0c06b110a829e32893058e02fa317cdfff624437
Weiming Huang <weiming.huang36@myhunter.cuny.edu>
2020-06-10 08:52:47 -0400

    Added columns
    Mixed up cols and rows, fixed

--- script.js
+++ script.js
@@ -6,21 +6,35 @@ function addRow(){
     if (rows === 0) {
         let row = document.createElement("tr"); //creating a blank row
         let col = document.createElement("td"); //creating a col to add to blank row
-        col.appendChild(row); //adding a col to row which is then added to the grid
-        grid.appendChild(col);
+        row.appendChild(col); //adding a col to row which is then added to the grid
+        grid.appendChild(row);
         rows++;
         cols++;
     } else {
-        let col = document.createElement("td");
         let row = document.createElement("tr");
         for (let i = 0; i < cols; i++) {
-            col.appendChild(row);
-            rows++;
+            let col = document.createElement("td");
+            row.appendChild(col);
         }
-        grid.appendChild(col);
+        rows++;
+        grid.appendChild(row);
     }
 }
 
 function addCol(){
-
+    if (rows === 0) {
+        let row = document.createElement("tr"); //creating a blank row
+        let col = document.createElement("td"); //creating a col to add to blank row
+        row.appendChild(col); //adding a col to row which is then added to the grid
+        grid.appendChild(col);
+        rows++;
+        cols++;
+    } else {
+        let allRows = document.getElementsByTagName("tr"); //gets collection of all elements with tag name
+        for(let i = 0; i < rows; i++){
+            let col = document.createElement("td"); //creating a col
+            allRows[i].appendChild(col);    //adding col to the existing row
+        }
+        cols++;
+    }
 }
